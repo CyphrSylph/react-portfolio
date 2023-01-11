b527e521c70e0ec6eed51322e5de05e85b34f8df
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-11 16:44:32 -0600

    added home component and updated breakpoints

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 const App = () => {
   return (
     <>
       <Header/>
+      <main className='main'>
+        <Home />
+      </main>
     </>
   )
 }
