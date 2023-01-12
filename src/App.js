1f96c4a6e61f50aac2fe09e03407ef129bf6f999
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-11 19:08:31 -0600

    added about component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header/>
       <main className='main'>
         <Home />
+        <About />
       </main>
     </>
   )
