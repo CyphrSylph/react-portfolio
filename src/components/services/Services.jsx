e220b65a966c9345fff511f4a6d6cdffe9ee0836
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-12 18:33:51 -0600

    updated modals to include active toggle and added breakpoints

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './services.css';
 import { HiOutlineClipboardList, HiOutlineArrowSmRight, HiOutlineCheckCircle, HiX } from 'react-icons/hi';
 
 const Services = () => {
-  return (
+    const [toggleState, setToggleState] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
+    return (
     <section className="services section" id="services">
         <h2 className="section__title">Services</h2>
         <span className="section__subtitle">What I offer</span>
@@ -14,13 +20,13 @@ const Services = () => {
                     <HiOutlineClipboardList className="services__icon" />
                     <h3 className="services__title">A<br />Service Title</h3>
                 </div>
-                <span className="services__button">
-                    View More{" "}
+                <span className="services__button" onClick={() => toggleTab(1)}>
+                    View More
                     <HiOutlineArrowSmRight className="services__button-icon" />
                 </span>
-                <div className="services__modal">
+                <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <HiX className="services__modal-close" />
+                        <HiX onClick={() => toggleTab(0)} className="services__modal-close" />
                         <h3 className="services__modal-title">A Service Title</h3>
                         <p className="services__modal-description">
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit incidunt fugiat ipsa autem ipsum, voluptatem sunt facere error dolore corporis cumque quia laboriosam.
@@ -54,13 +60,13 @@ const Services = () => {
                     <HiOutlineClipboardList className="services__icon" />
                     <h3 className="services__title">B<br />Service Title</h3>
                 </div>
-                <span className="services__button">
-                    View More{" "}
+                <span className="services__button" onClick={() => toggleTab(2)}>
+                    View More
                     <HiOutlineArrowSmRight className="services__button-icon" />
                 </span>
-                <div className="services__modal">
+                <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <HiX className="services__modal-close" />
+                        <HiX onClick={() => toggleTab(0)} className="services__modal-close" />
                         <h3 className="services__modal-title">B Service Title</h3>
                         <p className="services__modal-description">
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit incidunt fugiat ipsa autem ipsum, voluptatem sunt facere error dolore corporis cumque quia laboriosam.
@@ -94,13 +100,13 @@ const Services = () => {
                     <HiOutlineClipboardList className="services__icon" />
                     <h3 className="services__title">C<br />Service Title</h3>
                 </div>
-                <span className="services__button">
-                    View More{" "}
+                <span className="services__button" onClick={() => toggleTab(3)}>
+                    View More
                     <HiOutlineArrowSmRight className="services__button-icon" />
                 </span>
-                <div className="services__modal">
+                <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <HiX className="services__modal-close" />
+                        <HiX onClick={() => toggleTab(0)} className="services__modal-close" />
                         <h3 className="services__modal-title">C Service Title</h3>
                         <p className="services__modal-description">
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit incidunt fugiat ipsa autem ipsum, voluptatem sunt facere error dolore corporis cumque quia laboriosam.
