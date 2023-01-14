73c19faae0817a407df923c1808b552f6828fdea
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-14 15:40:41 -0600

    added testimonial component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/testimonials/Testimonials';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
+        <Testimonials />
       </main>
     </>
   );
