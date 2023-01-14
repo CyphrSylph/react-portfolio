15c8b667287a13a00fa2c7860068e9e6025bd15d
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-13 20:43:46 -0600

    added qualification component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </>
   );
