fdac0cc6b105270ba8fb844a29f6e9ffef160794
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-13 20:44:28 -0600

    initiated qualification component with tab feature

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import "./qualification.css";
+import { HiOutlineAcademicCap, HiOutlineBriefcase, HiOutlineCalendar } from "react-icons/hi";
+
+const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1)
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+  
+    return (
+    <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__subtitle">My Journey</span>
+
+        <div className="qualification__container container">
+            <div className="qualification__tabs">
+                <div className={toggleState === 1 ? "qualification__button button--flex qualification__active" 
+                    : "qualification__button button--flex"} onClick={() => toggleTab(1)}>
+                    <HiOutlineAcademicCap className="qualification__icon" />
+                    Education
+                </div>
+                <div className={toggleState === 2 ? "qualification__button button--flex qualification__active" 
+                    : "qualification__button button--flex"} onClick={() => toggleTab(2)}>
+                    <HiOutlineBriefcase className="qualification__icon" />
+                    Experience
+                </div>
+            </div>
+
+            <div className="qualification__sections">
+                <div className={toggleState === 1 ? "qualification__content qualification__content-active"
+                    : "qualification__content"}>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Fine Arts</h3>
+                            <span className="qualification__subtitle">St. Philips College</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2010-2012
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">Design</h3>
+                            <span className="qualification__subtitle">San Antonio College</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2013-2016
+                            </div>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Web Development</h3>
+                            <span className="qualification__subtitle">Codeup</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                May-Oct 2022
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                </div>
+                <div className={toggleState === 2 ? "qualification__content qualification__content-active"
+                    : "qualification__content"}>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Tech Support</h3>
+                            <span className="qualification__subtitle">PC Outlet - San Antonio, TX</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2012-2015
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">Mac+ Advisor</h3>
+                            <span className="qualification__subtitle">VMC/Apple - San Antonio, TX</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2015-2016
+                            </div>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Logistics Manager</h3>
+                            <span className="qualification__subtitle">ENV Services - San Antonio, TX</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2016-2022
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Digital Illustrator</h3>
+                            <span className="qualification__subtitle">Freelance - Remote, TX</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2019-Present
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
+  );
+}
+
+export default Qualification;
