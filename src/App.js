f62dc09e18e51b02a9214f9d3be95d6c32d502a8
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-14 19:02:07 -0600

    added footer component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Qualification />
         <Testimonials />
         <Contact />
+        <Footer />
       </main>
     </>
   );
