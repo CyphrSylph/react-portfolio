6937beb5a000b794f9fc958726ef0eb8c1fff334
kmaalig <106278777+kmaalig@users.noreply.github.com>
2023-01-15 21:47:21 -0600

    added scroll and active state to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,14 @@ import { HiOutlineHome, HiOutlineUser, HiOutlineBadgeCheck, HiOutlineClipboardLi
 
 
 const Header = () => {
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector(".header");
+        if(this.scrollY >= 80) header.classList.add("scroll-header");
+        else header.classList.remove("scroll-header");
+    });
+   
     const[Toggle, showMenu] = useState(false);
+    const[activeNav, setActiveNav] = useState("#home");
 
     return(
         <header className="header">
@@ -13,32 +20,38 @@ const Header = () => {
                 <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="#home" className="nav__link active-link">
+                            <a href="#home" onClick={() => setActiveNav("#home")} 
+                            className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
                                 <HiOutlineHome className="nav__icon"/> Home
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
+                            <a href="#about" onClick={() => setActiveNav("#about")} 
+                            className={activeNav === "#about" ? "nav__link active-link" : "nav__link"}>
                                 <HiOutlineUser className="nav__icon"/> About
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#skills" className="nav__link">
+                            <a href="#skills" onClick={() => setActiveNav("#skills")} 
+                            className={activeNav === "#skills" ? "nav__link active-link" : "nav__link"}>
                                 <HiOutlineBadgeCheck className="nav__icon"/> Skills
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#services" className="nav__link">
+                            <a href="#services" onClick={() => setActiveNav("#services")} 
+                            className={activeNav === "#services" ? "nav__link active-link" : "nav__link"}>
                                 <HiOutlineClipboardList className="nav__icon"/> Services
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#portfolio" className="nav__link">
+                            <a href="#portfolio" onClick={() => setActiveNav("#portfolio")} 
+                            className={activeNav === "#portfolio" ? "nav__link active-link" : "nav__link"}>
                                 <HiOutlinePhotograph className="nav__icon"/> Portfolio
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#contact" className="nav__link">
+                            <a href="#contact" onClick={() => setActiveNav("#contact")} 
+                            className={activeNav === "#contact" ? "nav__link active-link" : "nav__link"}>
                                 <HiOutlineMail className="nav__icon"/> Contact
                             </a>
                         </li>
